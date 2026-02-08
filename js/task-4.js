7be99c1d30381a262e34a911ff89d82d6b96073c
vitaliifedunyk <vitalik.fedunyk@gmail.com>
2026-02-08 18:05:41 +0100

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const { elements } = event.currentTarget;
+    const emailValue = elements.email.value.trim();
+    const passwordValue = elements.password.value.trim();
+
+    if (!emailValue || !passwordValue) {
+        alert(`All form fields must be filled in`)
+    } else {
+        const info = {
+            email: emailValue,
+            password: passwordValue
+        };
+        console.log(info);
+        loginForm.reset()
+    }
+
+});

e66ee8269d1ce290ec112b5a5084e824bc045786
vitaliifedunyk <vitalik.fedunyk@gmail.com>
2026-02-08 17:23:24 +0100

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,12 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+
+const gallery = document.querySelector('.gallery')
+
+const markup = images
+  .map(({ url, alt }) => `<li><img src='${url}' alt='${alt}'></img></li>`)
+  .join('')
+
+gallery.insertAdjacentHTML('beforeend', markup)
